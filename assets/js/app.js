cb737233b2022a39aa0112f5d652e94943e37934
John <jduncanpark@gmail.com>
2017-09-16 15:19:09 -0400

    add logo

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
 	render() {
 		return(
 			<div style={{textAlign: 'center'}}>
-				<h1>Worldwide Shipping LLC!</h1>
+				<img src={require('../imgs/logo.png')} />
 				<Navbar collapseOnSelect>
 				    <Navbar.Header>
 				      <Navbar.Brand>
