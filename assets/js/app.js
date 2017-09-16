39ba23574b3b1b48760a1c850daf9fa007f968aa
John <jduncanpark@gmail.com>
2017-09-16 15:06:18 -0400

    get navbar skeleton up

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Nav, Navbar, NavItem, MenuItem } from 'react-bootstrap';
 
 export default class App extends React.Component {
 
@@ -7,6 +7,23 @@ export default class App extends React.Component {
 		return(
 			<div style={{textAlign: 'center'}}>
 				<h1>Worldwide Shipping LLC!</h1>
+				<Navbar collapseOnSelect>
+				    <Navbar.Header>
+				      <Navbar.Brand>
+				        <a href="#">Worldwide Shipping</a>
+				      </Navbar.Brand>
+				      <Navbar.Toggle />
+				    </Navbar.Header>
+				    <Navbar.Collapse>
+				      <Nav pullRight>
+				        <NavItem eventKey={1} href="#">Home</NavItem>
+				        <NavItem eventKey={2} href="#">Services</NavItem>
+				        <NavItem eventKey={3} href="#">Why Worldwide Shipping LLC?</NavItem>
+				        <NavItem eventKey={4} href="#">FAQ</NavItem>
+				        <NavItem eventKey={5} href="#">Contact Us</NavItem>
+				      </Nav>
+				    </Navbar.Collapse>
+				  </Navbar>
 			</div>);
 	}
 }
--- webpack.config.js
+++ webpack.config.js
@@ -12,11 +12,73 @@ module.exports = {
     port: 9000
   },
   module: {
-    loaders: [
-      { 
-        test: /\.js$/, 
-        loader: 'babel-loader', 
-        exclude: /node_modules/
+    rules: [
+      {
+        test: /\.js$/i,
+        use: [
+          {
+            loader: "babel-loader",
+          }
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
+      },
+      {
+        test:/\.png$/,
+        use: [
+          {
+            loader: "url-loader?limit=10000"
+          }
+        ]
+      },
+      {
+        test: /\.jpg$/,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ] 
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+        use : [
+          {
+            loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+          }
+        ]
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+        use : [
+          {
+            loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+          }
+        ]
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+        use : [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+        use : [
+          {
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+          }
+        ]
       }
     ]
   },
