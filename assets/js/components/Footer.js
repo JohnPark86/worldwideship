cf4ab7994b2fab2bab5f47abbe9dc058ffd2457e
John <jduncanpark@gmail.com>
2017-10-08 22:23:13 -0400

    rework of footer

--- assets/js/components/Footer.js
+++ assets/js/components/Footer.js
@@ -8,21 +8,21 @@ export default class Footer extends React.Component {
 		return(
 			<div className="footer">
 				<div className="footerFloatLeft ">
-					<img src={require('../../img/central.png')} />
-					<img src={require('../../img/fmcsa.gif')} />
-				</div>
-				<div className="footerFloatCenter">
-					<img className="payments" src={require('../../img/payment.png')} />
-					<div className="footerContactLeft">
+					<div className="footerContact">
+						<img className="payments" src={require('../../img/payment.png')} />
+					</div>
+					<div className="footerContact">
 						<p><b>Toll free:</b> 888-505-2851</p>
 						<p><b>Local:</b> 305-901-1410</p>
 					</div>
-					<div className="footerContactLeft">
+					<div className="footerContact">
 						<p><b>Fax:</b> 888-505-2853</p>
 						<p><b>MC #:</b> 934235</p>
 					</div>
 				</div>
 				<div className="footerFloatRight">
+				    <img src={require('../../img/central.png')} />
+					<img src={require('../../img/fmcsa.gif')} />
 					<img src={require('../../img/dept.png')} />
 					<img src={require('../../img/auth.png')} />
 				</div>
